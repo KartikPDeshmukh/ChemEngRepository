f8bd493d1771dd9ce17e73833237e3cfbcf973a7
KartikPDeshmukh <karthikeya_deshmukh@yahoo.com>
2025-12-14 12:21:54 +0000

    reorganisation

--- Phase_1/dev1.py
+++ Phase_1/dev1.py
@@ -3,20 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #INPUT CONDITIONS
-#Two inlet streams: A and B
+#Reaction scheme: aA + bB -> cC
+stoic_A = 1 #Stoichiometric coefficient of A
+M_A = 28 #kg/kmol Molar mass of species A
+stoic_B = 3 #Stoichiometric coefficient of B
+M_B = 2 #kg/kmol Molar mass of species B
+stoic_C = 2 #Stoichiometric coefficient of C
+M_C = 17 #kg/kmol Molar mass of species A
 
 #Stream A
 m_A = 1 #kg/s
-M_A = 10 #kg/kmol Molar mass of species A
 n_A = m_A / M_A #kmol/s Molar flow rate of species A
 c_A = 10 #Cost of species A in £/kg
 
 #Stream B
 m_B = 1 #kg/s
-M_B = 20 #kg/kmol Molar mass of species B
 n_B = m_B / M_B #Molar flow rate of species B
 c_B = 15 #Cost of species B in £/kg
 
+#OUTPUT CONDITIONS
 #Total
 m_tot_in = m_A + m_B
 n_tot_in = n_A + n_B
